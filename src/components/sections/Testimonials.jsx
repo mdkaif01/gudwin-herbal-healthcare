fdbcc9b31c296f43b68d93ba9541ec4686056aa8
mdkaif01 <md.kaif8573@gmail.com>
2026-08-10 19:11:57 +0530

    Updated testimonials id

--- src/components/sections/Testimonials.jsx
+++ src/components/sections/Testimonials.jsx
@@ -10,7 +10,7 @@ import { clientReviews } from "../../data";
 
 export default function Testimonials() {
   return (
-    <section className="py-20 bg-white">
+    <section id="testimonials" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
         {/* Heading */}
